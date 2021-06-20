a4a57ab087f149a84190705217a555c4b92fa28a
unknown <mdadalkhan@gmail.com>
2021-06-20 23:02:05 +0600

    OK

--- hello/main.cc
+++ hello/main.cc
@@ -3,14 +3,32 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
-    // Very big sum 
-    int tt;
-    long long sum = 0;
-    while(tt--){
-      long long num;
-      cin>>num;
-      sum = sum+num;
+    
+    int tCase; // number of test case;
+    int solutions; // final number of solution
+
+    cin>>tCase; // read test case
+    
+    while(tCase--){
+        int strLen = 0; // string length
+        int mathOne = 0; // 
+        string data;
+        
+        cin>>data;
+        
+        strLen = data.length();
+        
+        for(int i=0;i<strLen;i++){
+            int n = 0;
+            if(data[i] == '1') {
+                mathOne++;
+                n++;
+                if(n == 2 ) break;
+            }
+        }
+        solutions = mathOne / 2;
+        cout << solutions << '\n';
     }
-    cout<< sum;
+    
     return 0;
 }
